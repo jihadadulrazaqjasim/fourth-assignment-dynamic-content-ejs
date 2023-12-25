7ff7bd402ad1589545efa0d2fa09450df486bd6d
Jihad Abdulrazaq <jihadabdulrazaq@gmail.com>
2023-12-26 01:38:10 +0300

    changes

---
+++ app.js
@@ -0,0 +1,26 @@
+//packages
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
+
+//local files
+const usersRoutes = require('./routes/users')
+const homeRoute = require('./routes/home')
+
+//parse body
+app.use(bodyParser.urlencoded({extended:false}))
+
+//serving from public
+app.use(express.static(path.join(__dirname,'public')))
+
+//get routes
+app.use(usersRoutes.routes)
+app.use(homeRoute)
+
+//set template engine
+app.set('view engine','ejs')
+app.set('views','views')
+
+
+app.listen(3000,()=>{console.log('Server started on port 3000')})
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+const userList = []
+
+router.get('/users',(req, res)=>{
+  return res.render('users',{pageTitle:'Users',users:userList})
+})
+
+router.post('/users',(req, res)=>{
+    const username = req.body.username
+    userList.push({username:username})
+    return res.redirect('/users')
+})
+
+exports.routes = router
+exports.list = userList
